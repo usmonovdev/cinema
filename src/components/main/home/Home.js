eca785a654cd9509475899825a2b7d7eae5e0c8c
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-01-10 20:27:55 +0500

    api check

--- src/components/main/home/Home.js
+++ src/components/main/home/Home.js
@@ -5,28 +5,28 @@ const API_KEY = "917c387c9e20da3ba121bafdd8e7df79"
 
 function Home() {
     const [firstPage, setFirstPage] = useState({
-        name: "",
         img: ""
     })
     useEffect(() => {
         axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`)
-        .then((res) => {
-            // setFirstPage({
-            //     name: res.data.original_title,
-            //     img: `https://image.tmdb.org/t/p/w200/${res.data.backdrop_path}`
-            // })
-            console.log(res)
-        })
+            .then((res) => {
+                setFirstPage({
+                    img: `https://image.tmdb.org/t/p/w500/${res.data.results[2].backdrop_path}`
+                })
+                console.log(res)
+            })
     }, [])
-  return (
-    <>
-    <Navbar />
-    <div className="container">
-        <h1>{firstPage.name}</h1>
-        <img src={firstPage.img} alt="" />
-    </div>
-    </>
-  )
+    console.log(firstPage)
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <div className="ads">
+                    <img src={firstPage.img} alt="" />
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Home
