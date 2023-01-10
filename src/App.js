74cbd9c527e7c53a5c556daadfd97466faaed4a6
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-01-10 12:58:37 +0500

    Sass added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+
 function App() {
   return (
-    <div>App js</div>
+    <>
+    <Navbar />
+    <Footer/>
+    </>
   );
 }
 
