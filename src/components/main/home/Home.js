289fef0e2bc9813585e21121b65e55e9d3cb9cf3
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-01-11 13:26:23 +0500

    Top rated films image

--- src/components/main/home/Home.js
+++ src/components/main/home/Home.js
@@ -1,17 +1,20 @@
 import axios from 'axios'
+import "./home.scss"
 import React, { useEffect, useState } from 'react'
 import Navbar from '../../navbar/Navbar'
 const API_KEY = "917c387c9e20da3ba121bafdd8e7df79"
 
 function Home() {
     const [firstPage, setFirstPage] = useState({
-        img: ""
+        img: "",
+        title: ""
     })
     useEffect(() => {
         axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`)
             .then((res) => {
                 setFirstPage({
-                    img: `https://image.tmdb.org/t/p/w500/${res.data.results[2].backdrop_path}`
+                    img: `https://image.tmdb.org/t/p/w500/${res.data.results[2].backdrop_path}`,
+                    title: res.data.results[2].title
                 })
                 console.log(res)
             })
@@ -20,10 +23,8 @@ function Home() {
     return (
         <>
             <Navbar />
-            <div className="container">
-                <div className="ads">
-                    <img src={firstPage.img} alt="" />
-                </div>
+            <div className="ads">
+                <img src={firstPage.img} alt={firstPage.title} />
             </div>
         </>
     )
