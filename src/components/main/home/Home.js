90b8a843bdd48984d11836625d5917a53ccebc05
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-01-13 16:45:46 +0500

    Media Screen

--- src/components/main/home/Home.js
+++ src/components/main/home/Home.js
@@ -7,62 +7,57 @@ import { BiLeftArrowAlt, BiPlay, BiRightArrowAlt } from 'react-icons/bi'
 const API_KEY = "917c387c9e20da3ba121bafdd8e7df79"
 
 function Home() {
-    const [firstPage, setFirstPage] = useState({
-        img: "",
-        title: "",
-        release: "",
-        vote: "",
-        overview: "",
-        original_language: ""
-    })
-    const defaultImg = 2;
+    const [firstPage, setFirstPage] = useState([])
     useEffect(() => {
         axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
             .then((res) => {
-                setFirstPage({
-                    img: `https://image.tmdb.org/t/p/original/${res.data.results[defaultImg].backdrop_path}`,
-                    title: res.data.results[defaultImg].title,
-                    release: res.data.results[defaultImg].release_date,
-                    vote: res.data.results[defaultImg].vote_average,
-                    overview: res.data.results[defaultImg].overview,
-                    original_language: res.data.results[defaultImg].original_language
-                })
+                setFirstPage(res.data.results)
                 console.log(res)
             })
     }, [])
+    const custom = 0;
+    console.log(firstPage)
     return (
         <>
             <Navbar />
             <div className="ads">
-                <div className='bg-image' style={{
-                    backgroundImage: `url(${firstPage.img})`
-                }}>
-                    <div className='top-popular-home'>
-                        <div className='play-and-next'>
-                            <div>
-                                <button><BiLeftArrowAlt /></button>
-                                <button><BiRightArrowAlt /></button>
-                            </div>
-                            <div>
-                                <div className='play-watch-now'>Watch Now!</div>
-                                <button className='play-icon'><BiPlay /></button>
-                            </div>
-                        </div>  
-                        <div className='info-box'>
-                            <div className='title-vote-box'>
-                                <h1>{firstPage.title.length < 30 ? <>{firstPage.title}</> : <>{firstPage.title.slice(0, 20)}...</>}</h1>
-                                <div className='vote-average'>
-                                    <AiFillStar />{firstPage.vote}
+                {firstPage.map((data) => {
+                    return (
+                        <div className='bg-image' key={custom} style={{
+                            backgroundImage: `url("https://image.tmdb.org/t/p/original/${data.backdrop_path}")`
+                        }}>
+                            <div className='top-popular-home'>
+                                <div className='play-and-next'>
+                                    <div>
+                                        <button>
+                                            <BiLeftArrowAlt />
+                                        </button>
+                                        <button>
+                                            <BiRightArrowAlt />
+                                        </button>
+                                    </div>
+                                    <div>
+                                        <div className='play-watch-now'>Watch Now!</div>
+                                        <button className='play-icon'><BiPlay /></button>
+                                    </div>
+                                </div>
+                                <div className='info-box'>
+                                    <div className='title-vote-box'>
+                                        <h1>{data.title.length < 30 ? <>{data.title}</> : <>{data.title.slice(0, 20)}...</>}</h1>
+                                        <div className='vote-average'>
+                                            <AiFillStar />{data.vote_average}
+                                        </div>
+                                    </div>
+                                    <p className='info-release'>
+                                        {data.release_date}
+                                        <span>({data.original_language})</span>
+                                    </p>
+                                    <p className='info-overview'>{data.overview.slice(0, 40)}...</p>
                                 </div>
                             </div>
-                            <p className='info-release'>
-                                {firstPage.release}
-                                <span>({firstPage.original_language})</span>
-                            </p>
-                            <p className='info-overview'>{firstPage.overview.slice(0, 40)}...</p>
                         </div>
-                    </div>
-                </div>
+                    )
+                })}
             </div>
         </>
     )
