9a18b13af3b3f21e958c89643625db620d59eba4
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-01-13 19:10:38 +0500

    Swiper function added in Home

--- src/components/main/home/Home.js
+++ src/components/main/home/Home.js
@@ -8,31 +8,62 @@ const API_KEY = "917c387c9e20da3ba121bafdd8e7df79"
 
 function Home() {
     const [firstPage, setFirstPage] = useState([])
+    const [index, setIndex] = useState(0)
+    const [sliderTime, setSliderTime] = useState(10000)
+
     useEffect(() => {
         axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
             .then((res) => {
-                setFirstPage(res.data.results)
+                setFirstPage(res.data.results.slice(0, 10))
                 console.log(res)
             })
     }, [])
-    const custom = 0;
+
+    useEffect(() => {
+        const slide = firstPage.length - 1;
+        if (index < 0) {
+            setIndex(slide)
+        }
+        if (index > slide) {
+            setIndex(0)
+        }
+    }, [firstPage, index])
+
+    useEffect(() => {
+        const time = setInterval(() => {
+            setIndex(index + 1)
+        }, sliderTime);
+        return () => {
+            clearInterval(time)
+        }
+    })
+
     console.log(firstPage)
+    console.log(index)
     return (
         <>
             <Navbar />
             <div className="ads">
-                {firstPage.map((data) => {
+                {firstPage.map((data, indexSlide) => {
+                    const { id, backdrop_path, title, vote_average, release_date, original_language, overview } = data;
+                    let slide = "nextMovie";
+                    if (indexSlide == index) {
+                        slide = "activeMovie"
+                    }
+                    if (indexSlide == index - 1 || (index == 0 && indexSlide == firstPage.length - 1)) {
+                        slide = "lastMovie"
+                    }
                     return (
-                        <div className='bg-image' key={custom} style={{
-                            backgroundImage: `url("https://image.tmdb.org/t/p/original/${data.backdrop_path}")`
+                        <div className={`bg-image ${slide}`} key={id} style={{
+                            backgroundImage: `url("https://image.tmdb.org/t/p/original/${backdrop_path}")`
                         }}>
                             <div className='top-popular-home'>
                                 <div className='play-and-next'>
                                     <div>
-                                        <button>
+                                        <button onClick={() => setIndex(index - 1)}>
                                             <BiLeftArrowAlt />
                                         </button>
-                                        <button>
+                                        <button onClick={() => setIndex(index + 1)}>
                                             <BiRightArrowAlt />
                                         </button>
                                     </div>
@@ -43,16 +74,16 @@ function Home() {
                                 </div>
                                 <div className='info-box'>
                                     <div className='title-vote-box'>
-                                        <h1>{data.title.length < 30 ? <>{data.title}</> : <>{data.title.slice(0, 20)}...</>}</h1>
+                                        <h1>{title.length < 30 ? <>{title}</> : <>{title.slice(0, 20)}...</>}</h1>
                                         <div className='vote-average'>
-                                            <AiFillStar />{data.vote_average}
+                                            <AiFillStar />{vote_average}
                                         </div>
                                     </div>
                                     <p className='info-release'>
-                                        {data.release_date}
-                                        <span>({data.original_language})</span>
+                                        {release_date}
+                                        <span>({original_language})</span>
                                     </p>
-                                    <p className='info-overview'>{data.overview.slice(0, 40)}...</p>
+                                    <p className='info-overview'>{overview.slice(0, 40)}...</p>
                                 </div>
                             </div>
                         </div>
