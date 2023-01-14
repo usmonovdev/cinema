8172e0bd72cfe08fa0fa1d9ba0c50b7e07cff3d5
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-01-14 20:25:30 +0500

    Params

--- src/components/main/movies/MoviesOpen.js
+++ src/components/main/movies/MoviesOpen.js
@@ -12,10 +12,11 @@ function Movies() {
     const {moviesId} = useParams()
 
     useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/movie/${moviesId}?api_key=${API_KEY}`).then(data => {
-            setMovie(data.data)
-            console.log(data.data);
-        })
+        axios.get(`https://api.themoviedb.org/3/movie/${moviesId}?api_key=${API_KEY}`)
+            .then(data => {
+                setMovie(data.data)
+                console.log(data.data)
+            })
     }, [])
 
     useEffect(() => {
