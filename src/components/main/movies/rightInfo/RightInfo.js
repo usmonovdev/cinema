1ecdfe8a9286360642e89f4c5a7d08ee15be4335
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-01-15 18:10:21 +0500

    F**k the Error

--- src/components/main/movies/rightInfo/RightInfo.js
+++ src/components/main/movies/rightInfo/RightInfo.js
@@ -5,10 +5,10 @@ function RightInfo({ movie }) {
         <>
             <h1>{movie.original_title} ({movie.release_date})</h1>
             <div>
-                <p>{movie.release_date} ({movie.production_countries[0].iso_3166_1})</p>
-                <p>{movie.genres[0].name}</p>
-                <p>{movie.genres[1].name}</p>
-                <p>{movie.genres[3].name}</p>
+                {/* <p>{movie.release_date} ({movie.production_countries[0].iso_3166_1})</p> */}
+                {/* <p>{movie.genres[0].name}</p> */}
+                {/* <p>{movie.genres[1].name}</p> */}
+                {/* <p>{movie.genres[3].name}</p> */}
                 <p>{movie.runtime}</p>
                 <p>{movie.tagline}</p>
             </div>
