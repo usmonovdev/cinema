bf89fb24575049c8c898940c9cd8de8458232ad7
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-01-15 12:04:34 +0500

    Update MoviesOpen.js

--- src/components/main/movies/MoviesOpen.js
+++ src/components/main/movies/MoviesOpen.js
@@ -2,20 +2,21 @@ import "../home/home.scss"
 import "./moviesOpen.scss"
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import Navbar from '../../navbar/Navbar'
+import { useParams } from 'react-router-dom'
+// import Navbar from '../../navbar/Navbar'
 const API_KEY = "917c387c9e20da3ba121bafdd8e7df79"
-const IMAGE_LINK = "https://image.tmdb.org/t/p/original/"
+// const IMAGE_LINK = "https://image.tmdb.org/t/p/original/"
 
 function Movies() {
     const [movie, setMovie] = useState([])
-    const {moviesId} = useParams()
+    const { moviesId } = useParams()
+    console.log(typeof moviesId)
+    console.log(typeof movie)
 
     useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/movie/${moviesId}?api_key=${API_KEY}`)
-            .then(data => {
-                setMovie(data.data)
-                console.log(data.data)
+        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+            .then((res) => {
+                setMovie(res.data.results.slice(0, 10))
             })
     }, [])
 
@@ -23,9 +24,13 @@ function Movies() {
         document.title = `Movie - ${movie.original_title}`
     })
 
+    const filterMovie = movie.filter(obj => obj.id == moviesId);
+    console.log(filterMovie)
+    console.log(movie)
     return (
         <>
-            <Navbar />
+            <h1>id: {moviesId}</h1>
+            {/* <Navbar />
             {!movie.adult ? <>
                 <div className="ads">
                     <div className='bg-image' style={{
@@ -49,7 +54,7 @@ function Movies() {
                     <h1>sdasda</h1>
                     <Link to="/">Back</Link>
                 </div>
-            </>}
+            </>} */}
         </>
     )
 }
