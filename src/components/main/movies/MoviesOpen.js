30964623dfb90b3b0c3f3d5d2953e4ed3c478282
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-01-15 15:06:55 +0500

    I Love Error's

--- src/components/main/movies/MoviesOpen.js
+++ src/components/main/movies/MoviesOpen.js
@@ -5,8 +5,7 @@ import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import Navbar from '../../navbar/Navbar'
 const API_KEY = "917c387c9e20da3ba121bafdd8e7df79"
-const IMAGE_LINK = "https://image.tmdsb.org/t/p/original/"
-// get movie by id api https://api.themoviedb.org/3/movie/${param.moviesId}?api_key=${API_KEY}
+const IMAGE_LINK = "https://image.tmdb.org/t/p/original/"
 
 function Movies() {
     const [movie, setMovie] = useState([])
@@ -19,22 +18,19 @@ function Movies() {
             })
     }, [moviesId])
 
-    // useEffect(() => {
-    //     document.title = `Movie - ${movie.original_title}`
-    // })
+    useEffect(() => {
+        document.title = `Movie - ${movie.original_title}`
+    })
 
-    // const filterMovie = movie.filter(obj => obj.id == moviesId);
-    // console.log(filterMovie)
     console.log(movie)
     return (
         <>
-            {/* <h1>id: {moviesId}</h1> */}
             <Navbar />
             {!movie.adult ? <>
                 <div className="ads">
                     <div className='bg-image' style={{
                         opacity: "1",
-                        backgroundImage: `url(${IMAGE_LINK}${movie.backdrop_path})`
+                        backgroundImage: `url("${IMAGE_LINK}${movie.backdrop_path}")`
                     }}>
                         <div className="opened-movie-backdrop">
                             <div className="left-info">
@@ -42,7 +38,6 @@ function Movies() {
                                 <div className="open-image"></div>
                             </div>
                             <div className="right-info">
-                                <h1>{movie.tagline}</h1>
                             </div>
                         </div>
                     </div>
