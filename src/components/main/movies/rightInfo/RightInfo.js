454d7f0991892c754856408452463c2e9625272b
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-01-17 16:45:43 +0500

    MediaScreen

--- src/components/main/movies/rightInfo/RightInfo.js
+++ src/components/main/movies/rightInfo/RightInfo.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./rightInfo.scss"
+import { RxBookmark } from "react-icons/rx"
+import { Tooltip } from 'antd';
+import { AiOutlineHeart, AiOutlineStar } from 'react-icons/ai';
 
 function RightInfo({ movie }) {
     const { original_title, release_date, runtime, tagline, overview, production_countries } = movie;
@@ -12,12 +15,25 @@ function RightInfo({ movie }) {
         <>
             <div className="right-info">
                 <h1>{original_title} ({release_date?.slice(0, 4)})</h1>
-                <div className='right-info-box'>
+                <div className='right-info-small'>
                     <p>{release_date?.replaceAll("-", "/")} ({first}) •</p>
                     <p>{tagline} •</p>
                     <p>{hours}h {remainMinutes}min</p>
                 </div>
-                <p>{overview}</p>
+                <div className="right-overview-box">
+                    <p>{overview}</p>
+                </div>
+                <div className="events-box">
+                    <Tooltip placement="bottom" title={"Mark As Fovorite"} color={"#343434"}>
+                        <AiOutlineHeart className='events' />
+                    </Tooltip>
+                    <Tooltip placement="bottom" title={"Rate It!"} color={"#343434"}>
+                        <AiOutlineStar className='events' />
+                    </Tooltip>
+                    <Tooltip placement="bottom" title={"Add To Your Watchlist"} color={"#343434"}>
+                        <RxBookmark className='events' />
+                    </Tooltip>
+                </div>
             </div>
         </>
     )
