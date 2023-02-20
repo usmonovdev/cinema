c3bf54477dc9f407c06ac56db6c140038e51ca51
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-02-20 09:08:09 +0500

    Search bar

--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useReducer } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import axios from 'axios'
 import { reducer } from '../../assets/reducer'
-import { RiMicLine } from 'react-icons/ri'
+import { RiMicLine, RiMovie2Line } from 'react-icons/ri'
 import { Link } from 'react-router-dom'
+import { AiFillStar } from 'react-icons/ai'
+import { BiDollar } from "react-icons/bi"
 const API_KEY = "917c387c9e20da3ba121bafdd8e7df79"
 
 function Search() {
@@ -12,7 +14,6 @@ function Search() {
     }
 
     const [state, dispatch] = useReducer(reducer, initialState)
-    console.log(state.inputValue)
     const handleInput = (e) => {
         dispatch({
             type: "INPUT_VALUE",
@@ -29,7 +30,7 @@ function Search() {
                     newApiData: data.data.results
                 })
             })
-    }, []);
+    }, [state.inputValue]);
 
     return (
         <div className='search-result'>
@@ -39,44 +40,37 @@ function Search() {
                     className='input'
                     placeholder='Search...'
                     onChange={(e) => handleInput(e.target.value)}
+                    value={state.inputValue}
                 />
                 <RiMicLine />
             </div>
             <div className='bottom'>
-                {/* {state.apiData?.map((data) => {
-                    return (
-                        <p>{data.original_title}</p>
-                    )
-                })} */}
-                <ul>
-                    <li>
-                        <Link to="#">
-                            <h3>Spider Man</h3>
-                        </Link>
-                        <div>
-                            <p>vote average: 9.1</p>
-                            <p>Pul: 8</p>
-                        </div>
-                    </li>
-                    <li>
-                        <Link to="#">
-                            <h3>Spider Man</h3>
-                        </Link>
-                        <div>
-                            <p>vote average: 9.1</p>
-                            <p>Pul: 8</p>
-                        </div>
-                    </li>
-                    <li>
-                        <Link to="#">
-                            <h3>Spider Man</h3>
-                        </Link>
-                        <div>
-                            <p>vote average: 9.1</p>
-                            <p>Pul: 8</p>
-                        </div>
-                    </li>
-                </ul>
+                {state.apiData?.length > 0 ?
+                    <ul>
+                        {state.apiData?.map((data) => {
+                            const { id, name, title, popularity, vote_average, media_type } = data
+                            const newName = name?.length > "20" ? `${name.slice(0, 25)}...` : name
+                            const newTitle = title?.length > "20" ? `${title.slice(0, 25)}...` : title
+
+                            const imya = newName ? newName : newTitle
+                            return (
+                                <li key={id}>
+                                    <Link to={`${media_type == "movie" ? "movie" : "tv"}/${id}`}>
+                                        <h3>{imya}</h3>
+                                    </Link>
+                                    <div>
+                                        <p><BiDollar /> {popularity}k</p>
+                                        <p><AiFillStar /> {vote_average?.toString().slice(0, 3)}</p>
+                                    </div>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    : <div className='not-found'>
+                        <RiMovie2Line />
+                        <p>No items!</p>
+                    </div>
+                }
             </div>
         </div>
     )
