d3691654cba959df1ae3e708269818a530b97ccc
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-02-23 17:54:59 +0500

    Actors page bug fixed

--- src/components/main/actor/Actor.js
+++ src/components/main/actor/Actor.js
@@ -49,79 +49,82 @@ function Actor() {
             </> : <>
                 <Navbar />
                 <div className='container'>
-                    {actor.results?.length !== 0 ?
-                        <>
-                            <motion.ul
-                                className="trending"
-                                variants={container}
-                                initial="hidden"
-                                animate="visible"
-                                style={{ marginBottom: "50px" }}
-                            >
-                                {actor.results?.map((data) => {
-                                    const { id, profile_path, title, name, popularity } = data
-                                    return (
-                                        <motion.li
-                                            className="trending-movie-container"
-                                            variants={item}
-                                            key={id}
-                                        >
-                                            <div className="trending-movie-box">
-                                                <Image
-                                                    preview={false}
-                                                    src={`https://image.tmdb.org/t/p/${imgState.size}/${profile_path}`}
-                                                    alt={title}
-                                                    fallback={actorImage}
-                                                />
-                                                <div className="trending-movie-info">
-                                                    <div className="like-and-open">
-                                                        <div className='icon'>
-                                                            <AiOutlineHeart />
-                                                        </div>
-                                                        <Link to={`/actor/${id}`}>
-                                                            <div className='play'>
-                                                                <p>View</p>
+                    <div className="movies-box">
+                        {actor.results?.length !== 0 ?
+                            <>
+                                <motion.ul
+                                    className="trending"
+                                    variants={container}
+                                    initial="hidden"
+                                    animate="visible"
+                                    style={{ marginBottom: "50px" }}
+                                >
+                                    {actor.results?.map((data) => {
+                                        const { id, profile_path, title, name, popularity } = data
+                                        return (
+                                            <motion.li
+                                                className="trending-movie-container"
+                                                variants={item}
+                                                key={id}
+                                            >
+                                                <div className="trending-movie-box">
+                                                    <Image
+                                                        preview={false}
+                                                        src={`https://image.tmdb.org/t/p/${imgState.size}/${profile_path}`}
+                                                        alt={title}
+                                                        fallback={actorImage}
+                                                    />
+                                                    <div className="trending-movie-info">
+                                                        <div className="like-and-open">
+                                                            <div className='icon'>
+                                                                <AiOutlineHeart />
                                                             </div>
-                                                        </Link>
+                                                            <Link to={`/actor/${id}`}>
+                                                                <div className='play'>
+                                                                    <p>View</p>
+                                                                </div>
+                                                            </Link>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                            <div className='info'>
-                                                <div className='text-anim actor'>
-                                                    <p className={`${name.length > "14" ? "anim" : ""}`}>{name}</p>
+                                                <div className='info'>
+                                                    <div className='text-anim actor'>
+                                                        <p className={`${name.length > "14" ? "anim" : ""}`}>{name}</p>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </motion.li>
-                                    )
-                                })}
-                            </motion.ul>
-                            <div className='pagination'>
-                                <ConfigProvider
-                                    theme={{
-                                        token: {
-                                            colorPrimary: colorState.color,
-                                            colorTextBase: "#fff",
-                                            borderRadius: "0"
-                                        }
-                                    }}
-                                >
-                                    <Pagination
-                                        onChange={changePage}
-                                        defaultCurrent={1}
-                                        total={50}
-                                        pageSize={1}
-                                        showLessItems={false}
-                                    />
-                                </ConfigProvider>
+                                            </motion.li>
+                                        )
+                                    })}
+                                </motion.ul>
+                                <div className='pagination'>
+                                    <ConfigProvider
+                                        theme={{
+                                            token: {
+                                                colorPrimary: colorState.color,
+                                                colorTextBase: "#fff",
+                                                borderRadius: "0"
+                                            }
+                                        }}
+                                    >
+                                        <Pagination
+                                            onChange={changePage}
+                                            defaultCurrent={1}
+                                            total={50}
+                                            pageSize={1}
+                                            showLessItems={false}
+                                        />
+                                    </ConfigProvider>
+                                </div>
+                            </> :
+                            <div className="trendingNoItem">
+                                <div className='noItems'>
+                                    <RiMovie2Line />
+                                    <p>Items not found!</p>
+                                </div>
                             </div>
-                        </> :
-                        <div className="trendingNoItem">
-                            <div className='noItems'>
-                                <RiMovie2Line />
-                                <p>Items not found!</p>
-                            </div>
-                        </div>
-                    }
+                        }
+                    </div>
+
                 </div>
                 <Footer />
             </>}
