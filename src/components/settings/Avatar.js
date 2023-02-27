e065f14d10e1cca34f273e4f5ee221416864723f
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-02-27 16:40:22 +0500

    Update user photo

--- src/components/settings/Avatar.js
+++ src/components/settings/Avatar.js
@@ -1,13 +1,50 @@
+import React, { useContext, useEffect } from 'react'
 import { Image } from 'antd'
-import React from 'react'
-import { useContext } from 'react'
+import { updateProfile } from 'firebase/auth'
+import { doc, setDoc } from 'firebase/firestore'
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
 import { MdDeleteOutline } from 'react-icons/md'
 import { AuthContext } from '../../context/AuthContext/AuthContext'
+import { db, storage } from '../../context/AuthContext/Firebase'
 
 function Avatar() {
     const { currentUser } = useContext(AuthContext)
-    const handleUpdatePhotoURL = () => {
-
+    const handleUpdatePhotoURL = (e) => {
+        e.preventDefault()
+        const file = e.target[0].files[0]
+        const storageRef = ref(storage, currentUser.displayName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        console.log('Upload is running');
+                        break;
+                }
+            },
+            () => {
+                console.log("Image not uploaded!")
+            },
+            () => {
+                getDownloadURL(storageRef).then(async (downloadURL) => {
+                    await updateProfile(currentUser, {
+                        displayName: currentUser.displayName,
+                        photoURL: downloadURL
+                    });
+                    await setDoc(doc(db, "users", currentUser.uid), {
+                        uid: currentUser.uid,
+                        displayName: currentUser.displayName,
+                        email: currentUser.email,
+                        photoURL: downloadURL,
+                    });
+                });
+            }
+        );
     }
     return (
         <>
